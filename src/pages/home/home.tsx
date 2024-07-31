b1a61b3b0b2e953a651ef0510b31f97d7cc83cae
Daniel Possamai <daniel.possamai20@gmail.com>
2024-07-31 11:28:50 -0300

    Configurado o basico para o projeto e removido coisas que não vão ser usadas

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,7 @@
+import './home.scss'
+
+export default function Home(){
+    return <div>
+        <h1>Home</h1>
+    </div>
+}
---
+++ src/routes.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/home/home";
+
+
+
+
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    }
+  ]);
