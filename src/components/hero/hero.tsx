8b60f5373fb422e7ffa760ed328f2cefa4048c89
Daniel Possamai <daniel.possamai20@gmail.com>
2024-08-01 09:34:15 -0300

    Adicionado acumulador de links

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -26,18 +26,48 @@ export default function Hero() {
     return (
         <div className="hero">
             <div className="englobador">
+                <div className="menu">
+                    <div className="item">
+                        <a href="#" className="link">
+                            <span> Links Úteis </span>
+                            <svg viewBox="0 0 360 360" xmlSpace="preserve">
+                                <g id="SVGRepo_iconCarrier">
+                                    <path
+                                        id="XMLID_225_"
+                                        d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393 c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393 s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"
+                                    ></path>
+                                </g>
+                            </svg>
+                        </a>
+                        <div className="submenu">
+                            <div className="submenu-item">
+                                <a href="#" className="submenu-link"> Development </a>
+                            </div>
+                            <div className="submenu-item">
+                                <a href="#" className="submenu-link"> Design </a>
+                            </div>
+                            <div className="submenu-item">
+                                <a href="#" className="submenu-link"> Marketing </a>
+                            </div>
+                            <div className="submenu-item">
+                                <a href="#" className="submenu-link"> SEO </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <div className="container-infos">
                     <nav>
                         <ul>
-                        <li className={activeItem === 'conscientize' ? 'active' : ''} onClick={() => updateText(conscientize, conscientizeImg, 'conscientize')}>Conscientize-se</li>
+                            <li className={activeItem === 'conscientize' ? 'active' : ''} onClick={() => updateText(conscientize, conscientizeImg, 'conscientize')}>Conscientize-se</li>
                             <li className={activeItem === 'previna' ? 'active' : ''} onClick={() => updateText(previna, previnaImg, 'previna')}>Previna-se</li>
                             <li className={activeItem === 'capacite' ? 'active' : ''} onClick={() => updateText(capacite, capaciteImg, 'capacite')}>Capacite-se</li>
                         </ul>
                     </nav>
-                    <p>{ text }</p>
+                    <p>{text}</p>
                 </div>
                 <div className="container-img">
-                    <img src={ imageUrl } alt=" Imagem referente a informação do titulo " />
+                    <img src={imageUrl} alt=" Imagem referente a informação do titulo " />
                 </div>
             </div>
         </div>
